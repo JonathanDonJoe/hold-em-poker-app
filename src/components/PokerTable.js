03929eefc586051069179f5b2aeba66bca525f88
Jonathan Joe <JonathanDonJoe@gmail.com>
2019-09-09 15:36:27 -0400

    Added prepDeck method that deals each player 2 cards

--- src/components/PokerTable.js
+++ src/components/PokerTable.js
@@ -10,14 +10,39 @@ class PokerTable extends Component {
         this.deck.create();
         this.deck.shuffle();
         console.log(this.deck.cards)
+        this.state = {
+            player1Hand: {},
+            player2Hand: {},
+            communityHand: {}
+        }
+    }
+
+    prepDeck = () => {
+        // const burn = this.deck.cards.shift();
+        const card1 = this.deck.cards.shift();
+        const card2 = this.deck.cards.shift();
+        const card3 = this.deck.cards.shift();
+        const card4 = this.deck.cards.shift();
+
+        this.setState({
+            player1Hand: [card1, card3],
+            player2Hand: [card2, card4],
+            communityHand: this.deck.cards
+        })
     }
 
     render() { 
+        console.log(this.state.player1Hand)
+        console.log(this.state.player2Hand)
+        console.log(this.state.communityHand)
         return (
             <div className='the-table col-sm-12'> 
-                <PokerHand />
-                <PokerHand />
-                <PokerHand />
+                <PokerHand cards={this.state.player1Hand} />
+                <PokerHand cards={this.state.player2Hand} />
+                <PokerHand cards={this.state.communityHand} />
+                <button onClick={this.prepDeck} className='btn btn-primary'>
+                    Start
+                </button>
             </div>
         );
     }
